8eade276ab07b4756365d3bf1682f3f694fac271
Benedikt Vidic <benedikt.vidic@gmx.de>
2021-11-11 14:40:09 +0100

    added index page

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,6 +9,10 @@ class AuthorsController < ApplicationController
         redirect_to @author
     end
 
+    def index
+        @authors = Author.all
+    end
+
     def show
         @author = Author.find(params[:id])
     end
