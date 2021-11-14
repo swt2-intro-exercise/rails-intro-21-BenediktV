c018d3d66dd02e2f0948bea765230807e94ae297
Benedikt Vidic <benedikt.vidic@gmx.de>
2021-11-14 12:44:04 +0100

    added filtering by year

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,5 +3,7 @@ class Paper < ApplicationRecord
     validates :venue, presence: true
     validates :year, presence: true, numericality: true
 
+    scope :publishedIn, ->(year) { where("year = ?", year) }
+
     has_and_belongs_to_many :authors
 end
