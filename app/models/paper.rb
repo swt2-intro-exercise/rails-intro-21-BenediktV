7ae7626027dd1b56af856594d28337f1549d1063
Benedikt Vidic <benedikt.vidic@gmx.de>
2021-11-14 12:47:54 +0100

    added missing conditional

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,7 +3,7 @@ class Paper < ApplicationRecord
     validates :venue, presence: true
     validates :year, presence: true, numericality: true
 
-    scope :publishedIn, ->(year) { where("year = ?", year) }
+    scope :publishedIn, ->(year) { where("year = ?", year) if year.present?}
 
     has_and_belongs_to_many :authors
 end
